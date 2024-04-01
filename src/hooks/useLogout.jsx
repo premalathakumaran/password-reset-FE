4839a22f90b06b9d6e10b1bc1c86c187620af3e4
premalathakumaran <premalathakumaran18@gmail.com>
2024-04-01 15:13:07 +0530

    password fe commit

---
+++ src/hooks/useLogout.jsx
@@ -0,0 +1,14 @@
+
+
+import { useNavigate } from 'react-router-dom'
+
+function useLogout() {
+    let navigate = useNavigate()
+  return()=>{
+        sessionStorage.clear()
+        navigate('/login')
+    }
+}
+
+
+export default useLogout
